56f98842f6f66e1e5bba705f7dc647ac3d717b04
johnnyb88 <johnnybarratt@gmail.com>
2019-10-16 20:15:38 +0000

    add more data for each tables

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,7 +1,14 @@
 INSERT INTO users (name, email, password) VALUES ('Armand Hilll', 'lera_hahn@dickens.org', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 INSERT INTO users (name, email, password) VALUES ('Jacob', 'bobby_hafhn@dgmdd.org', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 INSERT INTO users (name, email, password) VALUES ('Troy McClure', 'youmightrememberme@movies.org', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
-INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, city, province, post_code) VALUES ('Bellview Hospital','description', 'https://www.avsforum.com/wordpress/wp-content/uploads/2019/02/Haunted-Hospital-1000x563.jpg','https://www.avsforum.com/wordpress/wp-content/uploads/2019/02/Haunted-Hospital-1000x563.jpg', 20, '123FakeSt', 12, 4, 5, 'Canada', 'Vancouver', 'BC', 'V9A16Z');
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, city, province, post_code) VALUES ('Bellview Hospital','description', 'https://www.avsforum.com/wordpress/wp-content/uploads/2019/02/Haunted-Hospital-1000x563.jpg','https://www.avsforum.com/wordpress/wp-content/uploads/2019/02/Haunted-Hospital-1000x563.jpg', 20, '123 Fake St.', 12, 4, 5, 'Canada', 'Vancouver', 'BC', 'V8F46Z');
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, city, province, post_code) VALUES ('Cozy Mansion','description', 'https://www.avsforum.com/wordpress/wp-content/uploads/2019/02/Haunted-Hospital-1000x563.jpg','https://www.avsforum.com/wordpress/wp-content/uploads/2019/02/Haunted-Hospital-1000x563.jpg', 50, '2343 Adanac Rd', 6, 3, 8, 'Canada', 'Vancouver', 'BC', 'V9H56Z');
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, city, province, post_code) VALUES ('Huge Cottage','description', 'https://www.avsforum.com/wordpress/wp-content/uploads/2019/02/Haunted-Hospital-1000x563.jpg','https://www.avsforum.com/wordpress/wp-content/uploads/2019/02/Haunted-Hospital-1000x563.jpg', 100, '6787 Wooooot St.', 3, 1, 1, 'Canada', 'Vancouver', 'BC', 'V34J6Z');
 INSERT INTO reservations (start_date, end_date) VALUES ('2018-02-12T08:00:00.000Z', '2018-04-20T07:00:00.000Z');
+INSERT INTO reservations (start_date, end_date) VALUES ('2018-04-12T09:00:00.000Z', '2018-05-20T011:00:00.000Z');
+INSERT INTO reservations (start_date, end_date) VALUES ('2018-03-12T11:00:00.000Z', '2018-07-20T08:00:00.000Z');
 INSERT INTO property_reviews (rating, message) VALUES (4, 'its haunted for sure');
+INSERT INTO property_reviews (rating, message) VALUES (2, 'not cozy at all');
+INSERT INTO property_reviews (rating, message) VALUES (5, 'this was actually a huge cottage');
+
 
